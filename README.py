56babd17a50ff9300a43d50f64a5893d40a5bb71
praveen-tripathi0 <99598525+praveen-tripathi0@users.noreply.github.com>
2022-02-13 18:58:19 +0530

    readme.py
    this is a rock paper scissor game enjoy

---
+++ README.py
@@ -0,0 +1,41 @@
+import random 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+possible_action=['rock','papper','scissor']
+user_action=input("choose your move 'rock' 'paper' 'scissors':  ")
+computer_action=(random.choice(possible_action))
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
+else:
+    print("invalid input")
+
+    exit()
+print(bcolors.WARNING + "MSG: thanks for playing" + bcolors.ENDC)
+
+
